edb1ed31a6824811f8c2ca0e033aef563748613e
Aniket Hevin <anikethevin@gmail.com>
2026-07-14 11:36:05 +0530

    Initial Commit

---
+++ Cleanify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleanify
+//
+//  Created by Hevin on 14/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
